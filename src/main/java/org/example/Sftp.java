8e6fb69fae3a069a6263b798ee7cbea7f330aa05
Simon Arapoglu <simon.arapoglu@anevis-solutions.com>
2023-12-08 11:13:52 +0100

    added workflow

--- src/main/java/org/example/Sftp.java
+++ src/main/java/org/example/Sftp.java
@@ -1,2 +1,25 @@
-package org.example;public class Sftp {
-}
+//package org.example;
+//
+//import com.jcraft.jsch.JSch;
+//import com.jcraft.jsch.JSchException;
+//import com.jcraft.jsch.Session;
+//
+//
+//public class Sftp {
+//
+//
+//    public static void f() throws JSchException {
+//
+//        String password = "aaaa";
+//
+//        JSch jsch = new JSch();
+//        Session session = jsch.getSession("ubuntu", "192.69.69.69", 22);
+//        // ruleid: jsch-hardcoded-secret
+//        session.setPassword("aaaaa");
+//        // ruleid: jsch-hardcoded-secret
+//        session.setPassword(password);
+//        session.connect();
+//
+//    }
+//
+//}
